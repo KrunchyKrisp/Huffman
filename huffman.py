6abf3625dc41f78bdf57a55fcd08a08f5d9d2c3c
KrunchyKrisp <m.damidavicius@gmail.com>
2023-11-21 18:38:15 +0200

    Added range to -b

--- huffman.py
+++ huffman.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     parser.add_argument('Source', help='Source file', type=argparse.FileType('rb'))
     parser.add_argument('-d', '--Destination', help='Destination file', type=argparse.FileType('wb'))
     parser.add_argument('-D', '--Decode', help='Decode flag', action='store_true')
-    parser.add_argument('-b', '--ByteSize', help='Size of a byte', default=8, type=int)
+    parser.add_argument('-b', '--ByteSize', help='Size of a byte', default=8, type=int, choices=range(2, 17))
     args = parser.parse_args()
     print(args)
