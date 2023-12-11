63bed0985a9271ae7bae8dd7ed825adce85a6cb6
KrunchyKrisp <m.damidavicius@gmail.com>
2023-12-11 14:54:08 +0200

    Minor refactoring

--- huffman.py
+++ huffman.py
@@ -4,6 +4,36 @@ from pathlib import Path
 
 encoded_file_extension = '.huff'
 
+
+def parse_args_decode(source: str, destination: str) -> (Path, str):
+    # check source extension
+    source = Path(source)
+    if source.suffix != encoded_file_extension:
+        print(f'Wrong source file extension for decoding: {source.suffix}')
+        print(f'Expected source file extension for decoding: {encoded_file_extension}')
+        exit()
+    # check destination
+    if destination is None:
+        destination = source.stem
+
+    return source, destination
+
+
+def parse_args_encode(source: str, destination: str) -> (Path, Path):
+    # assume name
+    source = Path(source)
+    if destination is None:
+        destination = source.stem + encoded_file_extension
+    destination = Path(destination)
+    # in case destination was not None, check extension
+    if destination.suffix != encoded_file_extension:
+        print(f'Wrong destination file extension for encoding: {destination.suffix}')
+        print(f'Expected destination file extension for encoding: {encoded_file_extension}')
+        exit()
+
+    return source, destination
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('Source', help='Source file')
@@ -13,36 +43,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     print(args)
-    source = Path(args.Source)
+    source = args.Source
     destination = args.Destination
 
-    # encoding
+    # Path, str (find out original extension from header)
     if args.Decode:
-        # check source extension
-        if source.suffix != encoded_file_extension:
-            print(f'Wrong source file extension for decoding: {source.suffix}')
-            print(f'Expected source file extension for decoding: {encoded_file_extension}')
-            exit()
-        # check destination
-        if destination is None:
-            # need to figure out original extension? maybe different flow overall
-            destination = source.stem + '.???'
-        destination = Path(destination)
-
-    # encoding
+        source, destination = parse_args_decode(source, destination)
+    # Path, Path (checked)
     else:
-        # assume name
-        if destination is None:
-            destination = source.stem + encoded_file_extension
-        destination = Path(destination)
-        # in case destination was not None, check extension
-        if destination.suffix != encoded_file_extension:
-            print(f'Wrong destination file extension for encoding: {destination.suffix}')
-            print(f'Expected destination file extension for encoding: {encoded_file_extension}')
-            exit()
-
-    # reading/writing
-    with source.open('rb') as s_file:
-        with destination.open('wb') as d_file:
-            while s_buffer := s_file.read(16):
-                print(s_buffer)
+        source, destination = parse_args_encode(source, destination)
